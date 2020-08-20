3e2a56129725712c56d036e0fe1dd64e051015d8
Rodrigo Miguel Rojas s3784466 <s3784466@student.rmit.edu.au>
2020-08-20 18:39:01 +1000

    Implemented Tutorial 1 React Demo Code

---
+++ FrontEnd/src/main/java/com/rmit/sept/thurs1830/group6/frontend/FrontendApplication.java
@@ -0,0 +1,13 @@
+package com.rmit.sept.thurs1830.group6.frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrontendApplication.class, args);
+    }
+
+}
---
+++ FrontEnd/src/test/java/com/rmit/sept/thurs1830/group6/frontend/FrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rmit.sept.thurs1830.group6.frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
