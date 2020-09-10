307290e8af801fc57abf8f3633d6193e961bc79c
WinstonLie <s3777969@student.rmit.edu.au>
2020-09-10 17:37:13 +0700

    Added a test for login

--- FrontEnd/frontend-react/src/components/pages/pages_tests/loginpage.test.js
+++ FrontEnd/frontend-react/src/components/pages/pages_tests/loginpage.test.js
@@ -6,13 +6,13 @@ import Loginpage from '../loginpage';
 
 Enzyme.configure({ adapter: new Adapter() });
 
-// test('Check email', () => {
-//     let wrapper;
-//     wrapper = Enzyme.shallow(<Loginpage></Loginpage>);
-//
-//     wrapper.find('input[type="text"]').simulate('change', {target: {name: 'email', value: 'winstonl3900@gmail.com'}});
-//     expect(wrapper.state('email')).toEqual('winstonl3900@gmail.com');
-// })
+test('Check email', () => {
+    let wrapper;
+    wrapper = Enzyme.shallow(<Loginpage></Loginpage>);
+
+    wrapper.find('input[type="text"]').simulate('change', {target: {name: 'email', value: 'winstonl3900@gmail.com'}});
+    expect(wrapper.state('email')).toEqual('winstonl3900@gmail.com');
+})
 
 /*
     Check if render in loginpage works
