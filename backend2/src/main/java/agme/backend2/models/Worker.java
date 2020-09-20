7b2ec5b3fbd9091271e5c246aeccbe2e753b615f
rmit-s3589310-david-blanch <s3589310@student.rmit.edu.au>
2020-09-20 12:42:54 +1000

    Removing unused code

--- backend2/src/main/java/agme/backend2/models/Worker.java
+++ backend2/src/main/java/agme/backend2/models/Worker.java
@@ -1,16 +1,9 @@
 package agme.backend2.models;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.MapsId;
-import javax.persistence.OneToOne;
-import javax.persistence.PrimaryKeyJoinColumn;
 
 @Entity
 public class Worker {
