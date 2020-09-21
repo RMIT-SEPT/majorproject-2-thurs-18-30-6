ecfb6ac81233a70a4438d9f694a69550a7545fe0
rmit-s3589310-david-blanch <s3589310@student.rmit.edu.au>
2020-09-21 17:24:16 +1000

    Debugging

--- backend2/src/main/java/agme/backend2/services/UserServiceImpl.java
+++ backend2/src/main/java/agme/backend2/services/UserServiceImpl.java
@@ -77,6 +77,7 @@ public class UserServiceImpl implements UserService {
 		newManagement.setAdminId(adminId);
 		newManagement.setWorkerId(newUser.getUserId());
 		managementRepository.save(newManagement);
+		populateWorkerInformation(username);
 		return newUser;
 	}
 
