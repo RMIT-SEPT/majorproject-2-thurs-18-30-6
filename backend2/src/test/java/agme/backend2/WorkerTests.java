805e5c493952a0b5c83158c0bc72ee0b8b1215b8
rmit-s3589310-david-blanch <42173876+rmit-s3589310-david-blanch@users.noreply.github.com>
2020-09-22 23:33:30 +1000

    Update WorkerTests.java

--- backend2/src/test/java/agme/backend2/WorkerTests.java
+++ backend2/src/test/java/agme/backend2/WorkerTests.java
@@ -33,18 +33,21 @@ class WorkerTests {
 		userService.deleteAll();
 	}
 	
+	//Checks if registering a worker passes without calling an exception
 	@Test
 	void registerWorker() {
 		userService.registerWorker("testworkerFirstName", "testworkerLastName", "testworkerUsername", "testworkerPassword", "testworkerPassword", "testworkerAddress",
 				"testworkerPhone","testworker", validAdmin.getUserId());
 	}
 	
+	//Checks if worker login is successful
 	@Test
 	void validateWorkerSuccess() {
 		validWorker = userService.validateUser("workerUsername", "workerPassword");
 		assertEquals("workerFirstName",validWorker.getFirstName());
 	}
 	
+	//Checks if entering an incorrect username and password calls an exception
 	@Test
 	void validateWorkerFail() {
 		Assertions.assertThrows(ValidationException.class, ()->{
@@ -53,6 +56,7 @@ class WorkerTests {
 		});
 	}
 	
+	//Checks if trying to create a duplicate username calls an exception
 	@Test
 	void uniqueUsernameFailure() {
 		Assertions.assertThrows(ValidationException.class, ()->{
@@ -61,6 +65,7 @@ class WorkerTests {
 		});
 	}
 	
+	//Checks if all workers have a unique id
 	@Test
 	void uniqueId() {
 		userService.registerWorker("testworkerFirstName", "testworkerLastName", "testworkerUsername", "testworkerPassword", "testworkerPassword", "testworkerAddress",
